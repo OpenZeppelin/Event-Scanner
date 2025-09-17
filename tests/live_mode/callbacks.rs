ee779858296e7fc1d36bf3c5cfc28c3042eda371
0xNeshi <nenad.misic@openzeppelin.com>
2025-09-17 19:07:23 +0200

    fail_times -> max_fail_times

--- tests/live_mode/callbacks.rs
+++ tests/live_mode/callbacks.rs
@@ -66,7 +66,7 @@ async fn callbacks_failure_then_retry_success() -> anyhow::Result<()> {
     let callback = Arc::new(FlakyCallback {
         attempts: attempts.clone(),
         successes: successes.clone(),
-        fail_times: 2,
+        max_ fail_times: 2,
     });
 
     let filter = EventFilter {
