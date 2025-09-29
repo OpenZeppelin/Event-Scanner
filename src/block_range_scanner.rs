bdfddcfa13dd29327ca75d72aec114b497a4e784
Leo <leonard.paturel@openzeppelin.com>
2025-09-29 13:08:36 +0200

    merge

--- src/block_range_scanner.rs
+++ src/block_range_scanner.rs
@@ -128,10 +128,7 @@ pub enum Error {
     #[error("WebSocket connection failed after {0} attempts")]
     WebSocketConnectionFailed(usize),
 
-    #[error("End of block batch")]
-    Eof,
-
-    #[error("Reorg detected")]
+    #[error("Reorg Detected")]
     ReorgDetected,
 }
 
@@ -447,7 +444,9 @@ impl<N: Network> Service<N> {
 
         self.sync_historical_data(start_block, end_block).await?;
 
-        info!("Successfully synced historical data");
+        _ = self.subscriber.take();
+
+        info!("Successfully synced historical data, closing the stream");
 
         Ok(())
     }
@@ -554,12 +553,6 @@ impl<N: Network> Service<N> {
         }
 
         info!(batch_count = batch_count, "Historical sync completed");
-
-        if let Some(sender) = &self.subscriber &&
-            sender.send(Err(Error::Eof)).await.is_err()
-        {
-            warn!("Subscriber channel closed, cleaning up");
-        }
 
         Ok(())
     }
