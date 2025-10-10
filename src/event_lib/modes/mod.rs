5ab0b174a118227a990dec19ada22a9c36f55852
Leo <leonard.paturel@openzeppelin.com>
2025-10-10 12:00:24 +0900

    remove event filter from scanner

--- src/event_lib/modes/mod.rs
+++ src/event_lib/modes/mod.rs
@@ -8,7 +8,7 @@ pub use historic::{HistoricModeConfig, HistoricModeScanner};
 pub use live::{LiveModeConfig, LiveModeScanner};
 pub use sync::{SyncModeConfig, SyncModeScanner};
 
-use crate::{block_range_scanner::BlockRangeScanner, event_lib::EventFilter};
+use crate::block_range_scanner::BlockRangeScanner;
 
 pub struct EventScanner;
 
@@ -36,13 +36,12 @@ impl EventScanner {
 
 #[derive(Clone)]
 pub struct BaseConfig {
-    event_filters: Vec<EventFilter>,
     block_range_scanner: BlockRangeScanner,
 }
 
 impl BaseConfig {
     fn new() -> Self {
-        Self { event_filters: Vec::new(), block_range_scanner: BlockRangeScanner::new() }
+        Self { block_range_scanner: BlockRangeScanner::new() }
     }
 }
 
@@ -50,25 +49,8 @@ pub trait BaseConfigBuilder: Sized {
     fn base_mut(&mut self) -> &mut BaseConfig;
 
     #[must_use]
-    fn event_filter(mut self, filter: EventFilter) -> Self {
-        self.base_mut().event_filters.push(filter);
-        self
-    }
-
-    #[must_use]
-    fn event_filters(mut self, filters: Vec<EventFilter>) -> Self {
-        self.base_mut().event_filters.extend(filters);
-        self
-    }
-
-    #[must_use]
     fn max_reads(mut self, max: usize) -> Self {
         self.base_mut().block_range_scanner.max_read_per_epoch = max;
         self
     }
 }
-
-pub type Result<T> = std::result::Result<T, Error>;
-pub type Error = Box<dyn std::error::Error + Send + Sync>;
-
-pub struct EventStream;
