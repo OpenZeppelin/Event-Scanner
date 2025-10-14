8874cffc91da318bff2a65109bd643befe0887e2
0xNeshi <nenad.misic@openzeppelin.com>
2025-10-14 13:00:24 +0200

    ref: move LogMetadata to gated test_utils module

--- src/event_scanner.rs
+++ src/event_scanner.rs
@@ -25,6 +25,9 @@ use tokio::sync::{
 use tokio_stream::{StreamExt, wrappers::ReceiverStream};
 use tracing::{error, info, warn};
 
+#[cfg(any(test, feature = "test-utils"))]
+pub mod test_utils;
+
 pub struct EventScanner {
     block_range_scanner: BlockRangeScanner,
 }
@@ -68,53 +71,6 @@ impl<E: SolEvent> PartialEq<&[E]> for EventScannerMessage {
     fn eq(&self, other: &&[E]) -> bool {
         if let EventScannerMessage::Data(logs) = self {
             logs.iter().map(|l| l.data().clone()).eq(other.iter().map(SolEvent::encode_log_data))
-        } else {
-            false
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct LogMetadata<E: SolEvent> {
-    pub event: E,
-    pub address: alloy::primitives::Address,
-    pub tx_hash: alloy::primitives::B256,
-}
-
-impl<E: SolEvent> PartialEq<Vec<LogMetadata<E>>> for EventScannerMessage {
-    fn eq(&self, other: &Vec<LogMetadata<E>>) -> bool {
-        self.eq(&other.as_slice())
-    }
-}
-
-impl<E: SolEvent> PartialEq<&Vec<LogMetadata<E>>> for EventScannerMessage {
-    fn eq(&self, other: &&Vec<LogMetadata<E>>) -> bool {
-        self.eq(&other.as_slice())
-    }
-}
-
-impl<E: SolEvent, const N: usize> PartialEq<&[LogMetadata<E>; N]> for EventScannerMessage {
-    fn eq(&self, other: &&[LogMetadata<E>; N]) -> bool {
-        self.eq(&other.as_slice())
-    }
-}
-
-impl<E: SolEvent> PartialEq<&[LogMetadata<E>]> for EventScannerMessage {
-    fn eq(&self, other: &&[LogMetadata<E>]) -> bool {
-        if let EventScannerMessage::Data(logs) = self {
-            let log_data = logs
-                .iter()
-                .map(|l| {
-                    let address = l.address();
-                    let tx_hash = l.transaction_hash.unwrap();
-                    (l.inner.data.clone(), address, tx_hash)
-                })
-                .collect::<Vec<_>>();
-            let expected = other
-                .iter()
-                .map(|e| (e.event.encode_log_data(), e.address, e.tx_hash))
-                .collect::<Vec<_>>();
-            log_data == expected
         } else {
             false
         }
