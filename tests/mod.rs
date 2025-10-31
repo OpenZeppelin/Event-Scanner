93b308f4b873edea0e58dc2b9db7f610f5bb6918
0xNeshi <nenad.misic@openzeppelin.com>
2025-10-31 06:39:00 +0100

    test: move block_range_scanner integration tests to tests/

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,6 +1,7 @@
 // This test module triggers the `test-utils` feature when running
 // `cargo test`. Without it, you'd need to manually specify `--features test-utils`
 // every time.
+mod block_range_scanner;
 mod common;
 mod historic;
 mod latest_events;
