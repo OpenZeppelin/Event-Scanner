c291996309e614bb1d3efc22855a94c0df79544a
Nenad <nenad.misic@openzeppelin.com>
2025-11-05 14:49:26 +0100

    test: Reliability and Readability Improvements Mainly for Reorg Tests (#156)

--- tests/mod.rs
+++
@@ -1,9 +0,0 @@
-// This test module triggers the `test-utils` feature when running
-// `cargo test`. Without it, you'd need to manually specify `--features test-utils`
-// every time.
-mod block_range_scanner;
-mod common;
-mod historic;
-mod latest_events;
-mod live;
-mod sync;
