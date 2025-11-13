6b1b2e00eac63d9a49bac3c50cbabb5ffa306c6b
Leo <leonard.paturel@openzeppelin.com>
2025-11-13 19:17:13 +0900

    Refactor robust provider (#173)

---
+++ src/robust_provider/mod.rs
@@ -0,0 +1,9 @@
+pub mod builder;
+pub mod error;
+pub mod provider;
+pub mod provider_conversion;
+
+pub use builder::*;
+pub use error::Error;
+pub use provider::RobustProvider;
+pub use provider_conversion::{IntoProvider, IntoRobustProvider};
