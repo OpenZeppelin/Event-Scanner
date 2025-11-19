551041a2e838266f3dad3b4b71d9ca469c54ca49
Leo <leonard.paturel@openzeppelin.com>
2025-11-19 22:18:32 +0900

    fix: remove get block by id rpc
    f

--- src/robust_provider/provider.rs
+++ src/robust_provider/provider.rs
@@ -3,7 +3,6 @@ use std::{fmt::Debug, time::Duration};
 use alloy::{
     eips::{BlockId, BlockNumberOrTag},
     network::{Ethereum, Network},
-    primitives::BlockNumber,
     providers::{Provider, RootProvider},
     pubsub::Subscription,
     rpc::types::{Filter, Log},
@@ -61,25 +60,6 @@ impl<N: Network> RobustProvider<N> {
         }
 
         result?.ok_or_else(|| Error::BlockNotFound(number.into()))
-    }
-
-    /// Fetch a block number by Id with retry and timeout.
-    ///
-    /// # Errors
-    ///
-    /// See [retry errors](#retry-errors).
-    pub async fn get_block_by_id(&self, id: BlockId) -> Result<BlockNumber, Error> {
-        info!("get_block_by_id called");
-        let result = self
-            .retry_with_total_timeout(
-                |provider| async move { provider.get_block_number_by_id(id).await },
-                false,
-            )
-            .await;
-        if let Err(e) = &result {
-            error!(error = %e, "eth_getByBlockNumber failed");
-        }
-        result?.ok_or_else(|| Error::BlockNotFound(id))
     }
 
     /// Fetch a block number by Id with retry and timeout.
