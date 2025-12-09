82fee4c9bff3126943a86b3a2d11642b31184f5b
0xNeshi <nenad.misic@openzeppelin.com>
2025-12-09 12:22:36 +0100

    add historical_load_test

---
+++ examples/historical_load_test/main.rs
@@ -0,0 +1,110 @@
+use std::time::Instant;
+
+use alloy::{eips::BlockNumberOrTag, providers::ProviderBuilder, sol, sol_types::SolEvent};
+use alloy_node_bindings::Anvil;
+
+use event_scanner::{
+    EventFilter, EventScannerBuilder, Message, robust_provider::RobustProviderBuilder,
+};
+use futures::future::try_join_all;
+use tokio_stream::StreamExt;
+use tracing::{error, info};
+
+sol! {
+    // Built directly with solc 0.8.30+commit.73712a01.Darwin.appleclang
+    #[sol(rpc, bytecode="608080604052346015576101b0908161001a8239f35b5f80fdfe6080806040526004361015610012575f80fd5b5f3560e01c90816306661abd1461016157508063a87d942c14610145578063d732d955146100ad5763e8927fbc14610048575f80fd5b346100a9575f3660031901126100a9575f5460018101809111610095576020817f7ca2ca9527391044455246730762df008a6b47bbdb5d37a890ef78394535c040925f55604051908152a1005b634e487b7160e01b5f52601160045260245ffd5b5f80fd5b346100a9575f3660031901126100a9575f548015610100575f198101908111610095576020817f53a71f16f53e57416424d0d18ccbd98504d42a6f98fe47b09772d8f357c620ce925f55604051908152a1005b60405162461bcd60e51b815260206004820152601860248201527f436f756e742063616e6e6f74206265206e6567617469766500000000000000006044820152606490fd5b346100a9575f3660031901126100a95760205f54604051908152f35b346100a9575f3660031901126100a9576020905f548152f3fea2646970667358221220471585b420a1ad0093820ff10129ec863f6df4bec186546249391fbc3cdbaa7c64736f6c634300081e0033")]
+    contract Counter {
+        uint256 public count;
+
+        event CountIncreased(uint256 newCount);
+        event CountDecreased(uint256 newCount);
+
+        function increase() public {
+            count += 1;
+            emit CountIncreased(count);
+        }
+
+        function decrease() public {
+            require(count > 0, "Count cannot be negative");
+            count -= 1;
+            emit CountDecreased(count);
+        }
+
+        function getCount() public view returns (uint256) {
+            return count;
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let anvil = Anvil::new().block_time_f64(0.1).try_spawn()?;
+    let wallet = anvil.wallet();
+    let provider = ProviderBuilder::new()
+        .wallet(wallet.unwrap())
+        .connect(anvil.ws_endpoint_url().as_str())
+        .await?;
+    let contract = Counter::deploy(provider.clone()).await?;
+
+    let contract_address = contract.address();
+
+    let increase_filter = EventFilter::new()
+        .contract_address(*contract_address)
+        .event(Counter::CountIncreased::SIGNATURE);
+
+    println!("sending txs");
+    let mut txs = Vec::new();
+    for _ in 0..10_000 {
+        txs.push(contract.increase().send().await?);
+    }
+
+    println!("setting up watch futures");
+
+    let watch_futures = txs.into_iter().map(|pending| async move { pending.watch().await });
+
+    println!("executing watch futures");
+    try_join_all(watch_futures).await?;
+
+    println!("Setting up scanner");
+
+    let robust_provider = RobustProviderBuilder::new(provider)
+        .call_timeout(std::time::Duration::from_secs(30))
+        .max_retries(5)
+        .min_delay(std::time::Duration::from_millis(500))
+        .build()
+        .await?;
+
+    let mut scanner = EventScannerBuilder::historic()
+        .max_block_range(10)
+        .from_block(0)
+        .to_block(BlockNumberOrTag::Latest)
+        .connect(robust_provider)
+        .await?;
+
+    let mut stream = scanner.subscribe(increase_filter);
+
+    println!("Measuring...");
+
+    let start = Instant::now();
+
+    scanner.start().await?;
+
+    let mut log_count = 0;
+    while let Some(message) = stream.next().await {
+        match message {
+            Ok(Message::Data(logs)) => {
+                log_count += logs.len();
+            }
+            Ok(Message::Notification(info)) => {
+                info!("Received info: {:?}", info);
+            }
+            Err(e) => {
+                error!("Received error: {}", e);
+            }
+        }
+    }
+
+    println!("Finished historic in: {:?}  --- log count: {log_count}", start.elapsed());
+
+    Ok(())
+}
